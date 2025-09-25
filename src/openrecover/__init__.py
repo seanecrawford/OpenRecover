dcd74d95b3c3d76ccd360fee9efaad16f9ad7fa7
WTRS Labs <seanecrawford@yahoo.com>
2025-09-26 00:42:02 +0200

    Update __init__.py

--- src/openrecover/__init__.py
+++ src/openrecover/__init__.py
@@ -1,6 +1,2 @@
-# OpenRecover package marker
 __version__ = "0.7.0"
-
-# Don't import submodules here.
-# PyInstaller loads packages a bit differently; importing gui_qt/rawio/etc
-# at package import time can cause "module not found" during boot.
+# Do not import submodules here
